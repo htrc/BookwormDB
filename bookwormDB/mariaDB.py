5040e2c89938f10803122a4a0248333095ca8114
Deren Kudeki <dkudeki@illinois.edu>
2022-02-10 15:29:34 -0600

    We don't know we need to add the Lookup_ tables

--- bookwormDB/mariaDB.py
+++ bookwormDB/mariaDB.py
@@ -463,10 +463,10 @@ class Query(object):
                     else:
                         confirmed_groups.append(potential_group)
 
-                for confirmed_group in confirmed_groups:
-                    if confirmed_group != 'date_year':
-                        if confirmed_group + "heap_" in dicto['tables'] and confirmed_group + "Lookup_" not in dicto['tables']:
-                            dicto['tables'] += " NATURAL JOIN " + confirmed_group + "Lookup_"
+#                for confirmed_group in confirmed_groups:
+#                    if confirmed_group != 'date_year':
+#                        if confirmed_group + "heap_" in dicto['tables'] and confirmed_group + "Lookup_" not in dicto['tables']:
+#                            dicto['tables'] += " NATURAL JOIN " + confirmed_group + "Lookup_"
 
                 dicto['group_query'] = "GROUP BY " + ", ".join(confirmed_groups)
             logging.info("'{}'".format(dicto['group_query']))
