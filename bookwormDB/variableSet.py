4eebe15cdfa588115ebead94b405e229be782d2d
Deren Kudeki <dkudeki@illinois.edu>
2026-07-02 18:31:53 -0500

    Changes now allow for proper iteration

--- bookwormDB/variableSet.py
+++ bookwormDB/variableSet.py
@@ -3,7 +3,6 @@
 
 import json
 import os
-import sys
 import decimal
 import re
 from MySQLdb import escape_string
@@ -890,13 +889,12 @@ class variableSet(object):
         db.query("CREATE TABLE IF NOT EXISTS nwords (bookid INT UNSIGNED, PRIMARY KEY (bookid), nwords BIGINT);")
         db.query("UPDATE catalog JOIN nwords USING (bookid) SET catalog.nwords = nwords.nwords")
         db.query("CREATE TABLE IF NOT EXISTS temp_counts (bookid INT UNSIGNED, PRIMARY KEY (bookid), total_count BIGINT);")
-        chunk_size = 10
+        chunk_size = 100000
         catalog_size = 18696112
         smallest_bookid = 0
-        while smallest_bookid < 50:
+        while smallest_bookid < catalog_size:
             db.query("INSERT INTO temp_counts (bookid, total_count) SELECT bookid, SUM(count) FROM master_bookcounts WHERE bookid >= %s AND bookid < %s GROUP BY bookid;" % (smallest_bookid, smallest_bookid + chunk_size))
             smallest_bookid = smallest_bookid + chunk_size
-        sys.exit()
         db.query("INSERT INTO nwords (bookid, nwords) SELECT catalog.bookid, temp_counts.total_count FROM catalog LEFT JOIN nwords USING (bookid) JOIN temp_counts USING (bookid) WHERE nwords.bookid IS NULL;")
         db.query("DROP TABLE temp_counts;")
 #        db.query("INSERT INTO nwords (bookid,nwords) SELECT catalog.bookid,sum(count) FROM catalog LEFT JOIN nwords USING (bookid) JOIN master_bookcounts USING (bookid) WHERE nwords.bookid IS NULL GROUP BY catalog.bookid")
